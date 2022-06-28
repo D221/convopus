6e0c7ae572091ba41bdcbe9f508292a459173ac2
Benas <b.pvilavicius@gmail.com>
2022-06-28 17:15:45 +0300

    change program logic remove os.chdir()

--- convert-to-opus-cli/app.py
+++ convert-to-opus-cli/app.py
@@ -9,31 +9,28 @@ with open('config.json', 'r') as f:
 
 
 def convertFolder(PATH):
-    os.chdir(PATH)
     if config['KEEP']:
         KEEPLOCATION = os.path.join(PATH, 'original')
-        if not os.path.exists('original'):
-            os.makedirs('original')
+        os.makedirs(KEEPLOCATION, exist_ok=True)
     for FILENAME in os.listdir(PATH):
         if (FILENAME.endswith(tuple(config['COMMONTYPES']))):
-            WITHOUTEXT = os.path.splitext(FILENAME)[0]
-            OUTPUT = "".join([WITHOUTEXT, config['CONTAINER']])
-            ffpb.main(argv=['-i', FILENAME, '-vn', '-c:a',
-                            'libopus', '-b:a', config['BITRATE'], '-vbr', 'on', OUTPUT])
+            WITHOUT_EXT = os.path.splitext(FILENAME)[0]
+            INPUT = "".join([PATH, FILENAME])
+            OUTPUT = "".join([PATH, WITHOUT_EXT, config['CONTAINER']])
+            ffpb.main(argv=['-i', INPUT, '-vn', '-c:a', 'libopus',
+                      '-b:a', config['BITRATE'], '-vbr', 'on', OUTPUT])
             if config['KEEP']:
-                shutil.move('{0}/{1}'.format(PATH, FILENAME), KEEPLOCATION)
+                shutil.move(INPUT, KEEPLOCATION)
             else:
-                os.remove(FILENAME)
+                os.remove(INPUT)
         else:
             continue
 
 
 def convertFile(FILENAME):
-    WITHOUTEXT = os.path.splitext(FILENAME)[0]
-    FILEPATH = os.path.dirname(FILENAME)
-    os.chdir(FILEPATH)
-    OUTPUT = "".join([WITHOUTEXT, config['CONTAINER']])
+    WITHOUT_EXT = os.path.splitext(FILENAME)[0]
+    OUTPUT = "".join([WITHOUT_EXT, config['CONTAINER']])
     ffpb.main(argv=['-i', FILENAME, '-vn', '-c:a', 'libopus',
-                    '-b:a', config['BITRATE'], '-vbr', 'on', OUTPUT])
+              '-b:a', config['BITRATE'], '-vbr', 'on', OUTPUT])
     if not config['KEEP']:
         os.remove(FILENAME)
