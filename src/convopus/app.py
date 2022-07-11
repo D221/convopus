0a32b50faca87571027c677cfa8b0f86f4044e7f
D221 <b.pvilavicius@gmail.com>
2022-07-11 20:08:45 +0300

    The ability to chose settings using arguments

--- src/convopus/app.py
+++ src/convopus/app.py
@@ -4,28 +4,25 @@ import shutil
 import signal
 import sys
 
-from convopus.genconf import read_config
-
 from ffpb import ffpb
 
-config = read_config()
 
-
-def convert_folder(input_path):
+def convert_folder(input_path, config_bitrate, config_container, config_keep, config_vbr, config_common_types):
     '''For converting audio files in a folder.'''
-    if config['KEEP']:
+    if config_keep:
         keep_location = os.path.join(input_path, 'original')
         os.makedirs(keep_location, exist_ok=True)
+
     for file_name in sorted(os.listdir(input_path)):
         signal.signal(signal.SIGINT, signal_handler)
-        if file_name.endswith(tuple(config['COMMONTYPES'])):
+        if file_name.endswith(tuple(config_common_types)):
             without_ext = os.path.splitext(file_name)[0]
             input_file = os.path.join(input_path, file_name)
-            output_file_name = "".join([without_ext, config['CONTAINER']])
+            output_file_name = "".join([without_ext, config_container])
             output_file = os.path.join(input_path, output_file_name)
             ffpb.main(argv=['-i', input_file, '-vn', '-c:a', 'libopus',
-                            '-b:a', config['BITRATE'], '-vbr', 'on', output_file])
-            if config['KEEP']:
+                            '-b:a', config_bitrate, '-vbr', config_vbr, output_file])
+            if config_keep:
                 shutil.move(input_file, keep_location)
             else:
                 os.remove(input_file)
@@ -33,13 +30,13 @@ def convert_folder(input_path):
             continue
 
 
-def convert_file(file_name):
+def convert_file(file_name, config_bitrate, config_container, config_keep, config_vbr):
     '''For converting single audio file.'''
     without_ext = os.path.splitext(file_name)[0]
-    output_file = "".join([without_ext, config['CONTAINER']])
+    output_file = "".join([without_ext, config_container])
     ffpb.main(argv=['-i', file_name, '-vn', '-c:a', 'libopus',
-                    '-b:a', config['BITRATE'], '-vbr', 'on', output_file])
-    if not config['KEEP']:
+                    '-b:a', config_bitrate, '-vbr', config_vbr, output_file])
+    if not config_keep:
         os.remove(file_name)
 
 
--- src/convopus/main.py
+++ src/convopus/main.py
@@ -3,6 +3,9 @@ import argparse
 import os
 
 from convopus.app import convert_file, convert_folder
+from convopus.genconf import read_config
+
+config = read_config()
 
 
 def main():
@@ -11,11 +14,43 @@ def main():
         prog="convopus",
         description='A Python CLI program for converting audio files to opus')
     parser.add_argument('input', help='Input file or directory')
+    parser.add_argument(
+        '-b', '--bitrate', help='Prefered bitrate for audio files (default: 128k)')
+    parser.add_argument(
+        '-c', '--container', help='Container for audio files (.ogg, .opus, .oga, .mkv, .webm) (default: .ogg)')
+    parser.add_argument(
+        '-dk', '--dont-keep', help='Don\'t Keep original files (default: False)', action="store_true")
+    parser.add_argument('-v', '--vbr', help='Variable Bitrate (on/off) (default: on)',
+                        choices=["on", "off"], metavar="on/off")
     args = parser.parse_args()
+    if args.bitrate:
+        config_bitrate = args.bitrate
+    else:
+        config_bitrate = config['BITRATE']
+
+    if args.container:
+        config_container = args.container
+    else:
+        config_container = config['CONTAINER']
+
+    if args.dont_keep:
+        config_keep = False
+    else:
+        config_keep = config['KEEP']
+
+    if args.vbr:
+        config_vbr = args.vbr
+    else:
+        config_vbr = config['VBR']
+
+    config_common_types = config['COMMONTYPES']
+
     if os.path.isdir(args.input):
-        convert_folder(args.input)
+        convert_folder(args.input, config_bitrate, config_container,
+                       config_keep, config_vbr, config_common_types)
     elif os.path.isfile(args.input):
-        convert_file(args.input)
+        convert_file(args.input, config_bitrate,
+                     config_container, config_keep, config_vbr)
     else:
         print("The path/file is invalid!")
 
